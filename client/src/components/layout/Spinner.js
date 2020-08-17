fef1e66c6c9c2de878c470768c21f840523b5df0
Roshan Mishra <roshanmishrarinem@gmail.com>
2020-08-17 21:25:05 +0530

    Fix spinner

--- client/src/components/layout/Spinner.js
+++ client/src/components/layout/Spinner.js
@@ -1,12 +1,17 @@
 import React, { Fragment } from 'react';
 import spinner from './spinner.gif';
-
+import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css';
+import Loader from 'react-loader-spinner';
 export default () => (
-  <Fragment>
-    <img
-      src={spinner}
-      style={{ width: '200px', margin: 'auto', display: 'block' }}
-      alt='Loading...'
-    />
-  </Fragment>
+	<Fragment>
+		<div className='loader'>
+			<Loader
+				type='TailSpin'
+				color='#e07927'
+				height={100}
+				width={100}
+				timeout={3000} //3 secs
+			/>
+		</div>
+	</Fragment>
 );
